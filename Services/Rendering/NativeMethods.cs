9f4d2bc07057d8a71792ed7edaa9c707bdf89a85
St3venSmith <n1sittingyt69420@gmail.com>
2026-07-28 16:43:33 -0700

    Remove disabled-window input handling

--- Services/Rendering/NativeMethods.cs
+++ Services/Rendering/NativeMethods.cs
@@ -25,12 +25,6 @@ internal static class NativeMethods
 
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool EnableWindow(
-        IntPtr hwnd,
-        [MarshalAs(UnmanagedType.Bool)] bool enable);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
     internal static extern bool GetClientRect(
         IntPtr hwnd,
         out NativeRect rectangle);
