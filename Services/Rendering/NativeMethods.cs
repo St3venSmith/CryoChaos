cd6716a497f681232bedd5380f40149ef7fc8ce6
St3venSmith <n1sittingyt69420@gmail.com>
2026-07-28 17:08:19 -0700

    Support layered DX11 child window

--- Services/Rendering/NativeMethods.cs
+++ Services/Rendering/NativeMethods.cs
@@ -11,7 +11,9 @@ internal static class NativeMethods
     internal const int GWL_EXSTYLE = -20;
     internal const int WS_EX_TRANSPARENT = 0x20;
     internal const int WS_EX_TOOLWINDOW = 0x80;
+    internal const int WS_EX_LAYERED = 0x00080000;
     internal const int WS_EX_NOACTIVATE = 0x08000000;
+    internal const uint LWA_ALPHA = 0x00000002;
 
     internal const uint SWP_NOZORDER = 0x0004;
     internal const uint SWP_NOACTIVATE = 0x0010;
@@ -25,20 +27,19 @@ internal static class NativeMethods
 
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool GetClientRect(
+    internal static extern bool SetLayeredWindowAttributes(
         IntPtr hwnd,
-        out NativeRect rectangle);
+        uint colorKey,
+        byte alpha,
+        uint flags);
 
     [DllImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool SetWindowPos(
-        IntPtr hwnd,
-        IntPtr insertAfter,
-        int x,
-        int y,
-        int width,
-        int height,
-        uint flags);
+    internal static extern bool GetClientRect(IntPtr hwnd, out NativeRect rectangle);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool SetWindowPos(IntPtr hwnd, IntPtr insertAfter, int x, int y, int width, int height, uint flags);
 
     [DllImport("d3d11.dll", EntryPoint = "CreateDirect3D11DeviceFromDXGIDevice")]
     internal static extern int CreateDirect3D11DeviceFromDXGIDevice(IntPtr dxgiDevice, out IntPtr graphicsDevice);
